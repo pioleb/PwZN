6d825ee2e62f88b5c99b7e9e7aa9cb970e1651c1
pioleb <64978863+pioleb@users.noreply.github.com>
2023-02-04 23:54:21 +0100

    Add files via upload

---
+++ Lab4/scraping.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+req = requests.get('https://student.fizyka.pw.edu.pl/~pioleb/')
+
+#print(req.status_code)
+#print(req.text)
+#print(req.request.headers)
+
+soup = BeautifulSoup(req.text, 'html.parser')
+
+dane = soup.find('table')
+dane = dane.text
+
+print(dane)
+
+plik = "kot_dane.json"
+
+with open('Lab3/'+plik, 'w') as f:
+    json.dump(dane, f)
